8dcac847b5b08cb67e1d48fbca94fcf277d16d57
Flisan <me@f4n.dev>
2025-11-09 14:37:30 +0000

    dep: vanilla-extract を追加

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,10 @@
 // @ts-check
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	site: 'https://ijus.art',
+	vite: {
+		plugins: [vanillaExtractPlugin()],
+	},
 });
