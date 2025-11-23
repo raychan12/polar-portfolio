9640453fa97b3852adc5bc468cf8c8e5cab0e030
Flisan <me@f4n.dev>
2025-11-23 10:04:37 +0000

    feat: プロフィールセクションを実装

--- src/pages/_internal/components/ProfileSection.css.ts
+++ src/pages/_internal/components/ProfileSection.css.ts
@@ -2,8 +2,54 @@ import { style } from '@vanilla-extract/css';
 
 import { vars } from '../../../styles/theme.css';
 
+export const root = style({
+	display: 'flex',
+	flexDirection: 'column',
+	gap: '40px',
+});
+
+export const content = style({
+	display: 'flex',
+	gap: '40px',
+});
+
+export const avatar = style({
+	borderRadius: '100%',
+});
+
 export const name = style({
 	fontFamily: vars.font.jpMincho,
 	fontSize: '1.25em',
 	fontWeight: 400,
+	WebkitTextStroke: `1px ${vars.color.text.primary}`,
+});
+
+export const bio = style({
+	font: vars.text.jp.description,
+	fontWeight: 400,
+	lineHeight: '200%',
+	marginTop: '16px',
+});
+
+export const linksList = style({
+	display: 'flex',
+	flexWrap: 'wrap',
+	gap: '4px 32px',
+	marginTop: '8px',
+});
+
+export const link = style({
+	color: vars.color.brand.primary,
+	borderBottom: `1px solid transparent`,
+	transition: '150ms border-bottom-color',
+	fontSize: '24px',
+	selectors: {
+		'&:is(:hover, :focus-visible)': {
+			borderBottomColor: vars.color.brand.primary,
+		},
+	},
+});
+
+export const linkIcon = style({
+	verticalAlign: 'bottom',
 });
