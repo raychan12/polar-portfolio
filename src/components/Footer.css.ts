3a1ecabcfccc210a4e24b81ceebb18dc2ca67f1b
Flisan <me@f4n.dev>
2025-11-29 04:28:20 +0000

    chore: フッターの左右コンテンツが離れすぎないように

--- src/components/Footer.css.ts
+++ src/components/Footer.css.ts
@@ -3,12 +3,17 @@ import { style } from '@vanilla-extract/css';
 import { vars } from '../styles/theme.css';
 
 export const footer = style({
-	display: 'flex',
-	alignItems: 'stretch',
-	justifyContent: 'space-between',
 	background: vars.color.brand.primary,
 	padding: '40px 120px',
 	color: vars.color.text.tertiary,
+});
+
+export const container = style({
+	display: 'flex',
+	alignItems: 'stretch',
+	justifyContent: 'space-between',
+	maxWidth: '1200px',
+	marginInline: 'auto',
 });
 
 export const linkList = style({
