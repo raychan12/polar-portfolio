2bbcdb74c94cfd535b9e5e513a296d798474f732
Flisan <me@f4n.dev>
2025-11-29 05:50:45 +0000

    feat: ヘッダー付きのページでは PageLayout の上空白を短くする

--- src/layout/PageLayout.css.ts
+++ src/layout/PageLayout.css.ts
@@ -4,8 +4,10 @@ export const main = style({
 	flexGrow: 1,
 	width: '100%',
 	maxWidth: '1440px',
-	/* FIXME: padding-top は、ヘッダーがない場合 (トップページ) は 120px だけど、
-	 *				ヘッダーがある場合は 76px に縮こまる */
 	padding: '120px',
 	marginInline: 'auto',
+});
+
+export const smallTopPadding = style({
+	paddingTop: '80px',
 });
