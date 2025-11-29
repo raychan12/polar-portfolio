1ab2123eb34ca1f2f41e8a8649fc6c99dc067cf8
Flisan <me@f4n.dev>
2025-11-29 04:08:30 +0000

    chore: フッターのリンク一覧の実装を考慮して、名前をヘッダー用に変更

--- src/constant.ts
+++ src/constant.ts
@@ -3,14 +3,14 @@ export const COPYRIGHT_YEAR = 2025;
 export const CONTACT_FORM = 'https://forms.gle/vQ4eYDe7j1N4vt4H8';
 export const TWITTER = 'https://x.com/__ljus';
 
-export interface Link {
+export interface HeaderLink {
 	caption: string;
 	icon?: string;
 	linkTo: LinkTo;
 }
 export type LinkTo = { pathname: string; href?: undefined } | { pathname?: undefined; href: string };
 
-export const links: Link[] = [
+export const headerLinks: HeaderLink[] = [
 	{ caption: 'home', linkTo: { pathname: '/' } },
 	{ caption: 'works', linkTo: { pathname: '/works' } },
 	// { caption: "blog", link: { pathname: "/blog" }},
