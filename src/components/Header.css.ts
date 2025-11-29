a15e41221706ff889d003e81ff9280f351b2a394
Flisan <me@f4n.dev>
2025-11-29 03:44:03 +0000

    feat: スタイル設定 + 現在位置の印づけ + 外部リンク別タブ

--- src/components/Header.css.ts
+++ src/components/Header.css.ts
@@ -2,10 +2,23 @@ import { style } from '@vanilla-extract/css';
 
 import { vars } from '../styles/theme.css';
 
+export const root = style({
+	position: 'absolute',
+	right: '40px',
+	top: '40px',
+	color: 'white',
+});
+
 export const link = style({
 	fontFamily: vars.font.enSerif,
 	fontSize: '1.5em',
 	fontWeight: 600,
+	paddingInline: '10px',
+	selectors: {
+		'&[aria-current="page"]': {
+			borderBottom: `1px solid ${vars.color.text.tertiary}`,
+		},
+	},
 });
 
 export const iconLink = style({
