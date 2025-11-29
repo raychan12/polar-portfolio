b0d1fb5d19771d2de651f26f4df5ce2dd193c926
Flisan <me@f4n.dev>
2025-11-29 07:45:02 +0000

    feat: 戻るボタン

--- src/pages/works/works.css.ts
+++ src/pages/works/works.css.ts
@@ -1,5 +1,7 @@
 import { style } from '@vanilla-extract/css';
 
+import { vars } from '../../styles/theme.css';
+
 export const filters = style({
 	display: 'flex',
 	flexDirection: 'column',
@@ -11,4 +13,14 @@ export const worksList = style({
 	display: 'flex',
 	flexDirection: 'column',
 	gap: '40px',
+});
+
+export const backLink = style({
+	marginTop: '60px',
+	display: 'flex',
+	alignItems: 'center',
+	gap: '8px',
+	fontFamily: vars.font.en,
+	fontWeight: 300,
+	fontSize: '1em',
 });
