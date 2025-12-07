29e55172559e829cf2d29e8134a9a9208765d5b9
Flisan <me@f4n.dev>
2025-12-07 03:31:23 +0000

    feat: 作品タイプと制作形態を追加

--- src/context/work/types.ts
+++ src/context/work/types.ts
@@ -10,3 +10,9 @@ export type Work = {
 };
 
 export type WorkDate = { instant: Date; period?: never } | { instant?: never; period: { from: Date; to: Date } };
+
+export const workType = ['uiux', 'graphic', 'sosaku'] as const;
+export type WorkType = (typeof workType)[number];
+
+export const workContext = ['commission', 'collaboration', 'corporate', 'personal'] as const;
+export type WorkContext = (typeof workContext)[number];
