c8934e9743611ecbe04dccd327e842977e57f437
Flisan <me@f4n.dev>
2025-12-07 06:27:30 +0000

    fix: `headerLinks` をコンポーネント内に移動 + 存在し得ないフィールドは `never` に

--- src/components/types.ts
+++
@@ -1,6 +0,0 @@
-export type HeaderLink = {
-	caption: string;
-	icon?: string;
-	linkTo: LinkTo;
-};
-export type LinkTo = { pathname: string; href?: undefined } | { pathname?: undefined; href: string };
--- src/constant.ts
+++ src/constant.ts
@@ -1,15 +1,4 @@
-import type { HeaderLink } from './components/types';
-
 export const COPYRIGHT_YEAR = 2025;
 
 export const CONTACT_FORM = 'https://forms.gle/vQ4eYDe7j1N4vt4H8';
 export const TWITTER = 'https://x.com/__ljus';
-
-export const headerLinks: HeaderLink[] = [
-	{ caption: 'home', linkTo: { pathname: '/' } },
-	{ caption: 'works', linkTo: { pathname: '/works' } },
-	// { caption: "blog", link: { pathname: "/blog" }},
-	// { caption: "fee", link: { pathname: "/fee" }},
-	{ caption: 'contact', linkTo: { href: CONTACT_FORM } },
-	{ caption: 'Twitter', icon: 'twitter-outline', linkTo: { href: TWITTER } },
-];
