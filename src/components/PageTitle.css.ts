5e1628b3a5e7232a2c83f2086ec4fc2ba2bb2585
Flisan <me@f4n.dev>
2025-12-12 14:32:09 +0000

    feat: ページごとのヘッダーの画像最大幅を 1200px に制限

--- src/components/PageTitle.css.ts
+++ src/components/PageTitle.css.ts
@@ -4,24 +4,49 @@ import { vars } from '../styles/theme.css';
 
 export const root = style({
 	position: 'relative',
+	display: 'flex',
+	justifyContent: 'center',
+	overflow: 'hidden',
+});
+
+export const main = style({
+	position: 'relative',
+	width: '100%',
+	maxWidth: '1200px',
 	height: '200px',
-	padding: '56px 140px',
 	display: 'flex',
 	alignItems: 'end',
 });
 
-export const backgroundImage = style({
-	aspectRatio: 'unset',
+export const image = style({
 	position: 'absolute',
-	inset: 0,
+	objectPosition: 'left center',
+	objectFit: 'cover',
+	boxShadow: '0px 0px 32px 0px #00000029',
+	width: '100%',
+	height: '100%',
 	zIndex: -1,
+});
+
+export const background = style({
+	position: 'absolute',
+	inset: '0px',
+	width: '100%',
+	height: '100%',
+	aspectRatio: 'unset',
+	scale: 1.5,
+	objectFit: 'cover',
+	filter: 'blur(32px)',
+	zIndex: -2,
 });
 
 export const titleText = style({
 	width: 'fit-content',
 	padding: '0 20px 7px 10px',
+	margin: '56px 140px',
 	color: vars.color.text.tertiary,
 	fontFamily: vars.font.enSerif,
 	fontSize: '32px',
 	borderBottom: `1px solid ${vars.color.text.tertiary}`,
+	zIndex: 1,
 });
