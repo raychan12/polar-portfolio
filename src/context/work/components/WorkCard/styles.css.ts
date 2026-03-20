1950bce5f1464e0aafd40117355802afb691661f
Rintaro Itokawa <me@re-taro.dev>
2026-03-21 01:50:20 +0900

    feat: 既存の BreakPoint を deprecated に

--- src/context/work/components/WorkCard/styles.css.ts
+++ src/context/work/components/WorkCard/styles.css.ts
@@ -1,6 +1,6 @@
 import { createVar, fallbackVar, style } from '@vanilla-extract/css';
 
-import { BreakPoints, vars } from '../../../../foundation/styles/theme.css';
+import { DeprecatedBreakPoints, vars } from '../../../../foundation/styles/theme.css';
 
 // <a> タグの中に <a> はネストできないので、nav.tagsList の取り扱いに関して
 // この記事を参考にしています:
@@ -34,7 +34,7 @@ export const root = style({
 		},
 	},
 	'@media': {
-		[BreakPoints.SP]: {
+		[DeprecatedBreakPoints.SP]: {
 			gridTemplateColumns: 'max-content max-content minmax(0, 1fr)',
 			minHeight: '320px',
 		},
@@ -56,7 +56,7 @@ export const grid = style({
 		"img logo logo"
 	`,
 	'@media': {
-		[BreakPoints.SP]: {
+		[DeprecatedBreakPoints.SP]: {
 			gridTemplateAreas: `
 				"img logo ."
 				"img logo title"
@@ -77,11 +77,11 @@ export const visualImageContainer = style({
 	transform: 'translate(0)',
 	marginRight: '32px',
 	'@media': {
-		[BreakPoints.SP]: {
+		[DeprecatedBreakPoints.SP]: {
 			width: '227px',
 			marginRight: '40px',
 		},
-		[BreakPoints.TABLET]: {
+		[DeprecatedBreakPoints.TABLET]: {
 			marginRight: '80px',
 		},
 	},
@@ -129,7 +129,7 @@ export const smLogoSection = style({
 	width: '100%',
 	height: '100%',
 	'@media': {
-		[BreakPoints.SP]: {
+		[DeprecatedBreakPoints.SP]: {
 			display: 'none',
 		},
 	},
@@ -145,7 +145,7 @@ export const titleSection = style({
 	gridArea: 'title',
 	display: 'none',
 	'@media': {
-		[BreakPoints.SP]: {
+		[DeprecatedBreakPoints.SP]: {
 			display: 'flex',
 			flexDirection: 'column',
 			gap: '8px',
@@ -163,7 +163,7 @@ export const tagsList = style({
 	display: 'none',
 	font: vars.text.en.caption,
 	'@media': {
-		[BreakPoints.SP]: {
+		[DeprecatedBreakPoints.SP]: {
 			display: 'flex',
 			flexWrap: 'wrap',
 			gap: '8px 20px',
@@ -183,7 +183,7 @@ export const metaSection = style({
 	display: 'none',
 	gridArea: 'meta',
 	'@media': {
-		[BreakPoints.SP]: {
+		[DeprecatedBreakPoints.SP]: {
 			display: 'flex',
 			flexDirection: 'column',
 			gap: '8px',
